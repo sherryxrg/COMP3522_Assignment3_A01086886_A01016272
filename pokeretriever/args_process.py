d401ba9c41dbf40ab56fe95574a44fab70363c46
hsiehii <hsiehiisuan@gmail.com>
2020-04-09 23:44:27 -0700

    class ProcessRequest -
    get_pokedex_object()
    HTTP GET request from specified URL
    
    process_pokedex_object()
    uses asyncio.gather to run multiple coroutines GET requests concurrently

---
+++ pokeretriever/args_process.py
@@ -0,0 +1,48 @@
+import argparse
+import ssl
+import aiohttp
+import asyncio
+
+
+class ProcessRequest:
+
+    def __init__(self):
+        """
+        Contains the root url for getting pokemon moves.
+        An instance of this class contains a list that holds
+        the moves it has processed.
+
+        moves stores a list of PokemonMove objects.
+        """
+        # url needs /pokemon /move OR /ability
+        self.url = "https://pokeapi.co/api/v2/"
+        self.pokedex = []
+
+    async def get_pokedex_object(self, pokedex_obj, session: aiohttp.ClientSession) -> dict:
+        url = self.url + str(pokedex_obj)
+        response = await session.request(method="GET", url=url,
+                                         ssl=ssl.SSLContext())
+        json_response = await response.json()
+        return json_response
+
+    async def process_pokedex_object(self, objects: list):
+        """
+        This function depicts the use of asyncio.gather to run multiple
+        async coroutines concurrently. This allows us to execute multiple
+        HTTP GET requests concurrently and save time.
+        :param objects: a list of int or str, a list of pokemon ability id/names
+        """
+        async with aiohttp.ClientSession() as session:
+            list_urls = [obj for obj in objects]
+            coroutines = [self.get_pokedex_object(my_url, session) for my_url
+                          in list_urls]
+            responses = await asyncio.gather(*coroutines)
+            for response in responses:
+                # effect = response['effect_entries']
+                # ability = Ability(response['name'], int(response['id']),
+                #                   response['generation'], effect[0]['effect'], effect[0]['short_effect'], response['pokemon'])
+                # self.abilities.append(ability)
+                # ability_name = response['name']
+                # print(f"Ability: {ability_name.upper()}! {effect[0]['short_effect']}")
+
+
