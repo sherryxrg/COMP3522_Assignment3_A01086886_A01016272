5b1094aa41cd24eea2d04f90ed69571d21ed6896
hsiehii <hsiehiisuan@gmail.com>
2020-04-09 23:46:40 -0700

    class Request -
    cmd_requests()
    accepts and parses arguments from command line

--- pokeretriever/args_process.py
+++ pokeretriever/args_process.py
@@ -45,4 +45,57 @@ class ProcessRequest:
                 # ability_name = response['name']
                 # print(f"Ability: {ability_name.upper()}! {effect[0]['short_effect']}")
 
+                class Request:
 
+                    def __init__(self, mode: str, input_data: str,
+                                 expanded: bool,
+                                 input_file=None, output_file=None):
+                        self.mode = mode
+                        self.input_data = input_data
+                        self.expanded = expanded
+                        self.input_file = input_file
+                        self.output_file = output_file
+
+                def cmd_requests():
+                    parser = argparse.ArgumentParser()
+                    parser.add_argument('mode', type=str,
+                                        choices=['pokemon', 'ability', 'move'],
+                                        help="Mode application will be opened in")
+
+                    group = parser.add_mutually_exclusive_group(required=True)
+                    # todo: must have '.txt' extension
+                    group.add_argument('-if', '--inputfile',
+                                       help="File must have .txt extension.")
+                    # todo: id must be an int, and name must be str
+                    group.add_argument('-id', '--inputdata', type=int or str,
+                                       help="ID must be a digit, name must be a string.")
+
+                    parser.add_argument('-x', '--expanded',
+                                        action="store_true",
+                                        help="Only Pokemon queries support this mode. Default is False.")
+                    # todo: must have '.txt' extension
+                    parser.add_argument('-o', '--output', type=str,
+                                        help="File must have .txt extension.")
+
+                    # This parses all the arguments passed
+                    # through terminal/command line
+                    args = parser.parse_args()
+                    return args
+
+                def main():
+                    cmd_args = cmd_requests()
+                    if cmd_args.inputfile:
+                        input_data = cmd_args.inputfile
+                    else:
+                        input_data = cmd_args.inputdata
+
+                    # args are parsed into an instance of Request class
+                    req = Request(cmd_args.mode, input_data, cmd_args.expanded)
+
+                    # prints out command line arguments
+                    temp = vars(req)
+                    for item in temp:
+                        print(item, ':', temp[item])
+
+                if __name__ == '__main__':
+                    main()
