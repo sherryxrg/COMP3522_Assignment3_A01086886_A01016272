3b232552c223c4ad4cc711de79dfca47de08ffd1
hsiehii <hsiehiisuan@gmail.com>
2020-04-10 15:45:21 -0700

    added to string method for all PokedexObject classes

--- pokeretriever/pokedex_classes.py
+++ pokeretriever/pokedex_classes.py
@@ -23,12 +23,15 @@ class PokemonStat(PokedexObject):
         super().__init__(name, id_num)
         self.is_battle_only = is_battle_only
 
+    def __str__(self):
+        return f"\n*--- Pokemon Stats ---*" \
+               f"\nBattle Only: {self.is_battle_only}"
+
 
 class PokemonAbility(PokedexObject):
 
     def __init__(self, name: str, id_num: int, generation: str,
                  effect: str, short_effect: str, pokemon: list):
-
         super().__init__(name, id_num)
         self.generation = generation
         self.effect = effect
@@ -49,7 +52,6 @@ class PokemonMove(PokedexObject):
     def __init__(self, name: str, id_num: int, generation: str,
                  accuracy: int, pp: int, power: int,
                  move_type: str, damage_class: str, short_effect: str):
-
         super().__init__(name, id_num)
         self.generation = generation
         self.accuracy = accuracy
@@ -60,7 +62,12 @@ class PokemonMove(PokedexObject):
         self.short_effect = short_effect
 
     def __str__(self):
-        return f"{self.name}"
+        return f"\n*--- Pokemon Move ---*" \
+               f"\nname: {self.name}" \
+               f"\nid: {self.id_num}" \
+               f"\naccuracy: {self.accuracy}" \
+               f"\nmove type: {self.move_type}" \
+               f"\neffect: {self.short_effect}"
 
 
 class Pokemon(PokedexObject):
@@ -68,7 +75,6 @@ class Pokemon(PokedexObject):
     def __init__(self, name: str, id_num: int, height: int, weight: int,
                  stats: PokemonStat, types: list, abilities: PokemonAbility,
                  moves: PokemonMove):
-
         super().__init__(name, id_num)
         self.height = height
         self.weight = weight
@@ -77,5 +83,11 @@ class Pokemon(PokedexObject):
         self.abilities = abilities
         self.moves = moves
 
-
-
+    def __str__(self):
+        return f"\n*--- Pokemon ---*" \
+               f"\nname: {self.name}" \
+               f"\nid: {self.id_num}" \
+               f"\nstats: {self.stats}" \
+               f"\ntypes: {self.types}" \
+               f"\nabilities: {self.abilities}" \
+               f"\nmoves: {self.moves}"
